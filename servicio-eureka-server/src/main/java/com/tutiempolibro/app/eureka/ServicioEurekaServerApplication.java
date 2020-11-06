1f50eff4f34f866f721aad4f7cc65f32b80eb06b
Manuel Rios <marios@indracompany.com>
2020-11-06 12:03:33 -0500

    modificacion cadena de conexion bd mysql, install

--- servicio-eureka-server/src/main/java/com/tutiempolibro/app/eureka/ServicioEurekaServerApplication.java
+++ servicio-eureka-server/src/main/java/com/tutiempolibro/app/eureka/ServicioEurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.telefonica.pe.app.eureka;
+package com.tutiempolibro.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
